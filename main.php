c114437eadd4dec47bce78080199c9747bcc3f9c
Vezeveer <mortalmanny@live.com>
2023-01-14 15:09:02 +0800

    adjusted addproj form css

--- main.php
+++ main.php
@@ -201,7 +201,7 @@
                 </div>
             </div>
         </div>
-        <button type="button" class="btn btn-primary" data-target="#MymodalBack" data-toggle="modal" data-backdrop="static" data-keyboard="false">Open Modal</button>
+        <button id="btnAddProject" type="button" class="btn btn-primary" data-target="#MymodalBack" data-toggle="modal" data-backdrop="static" data-keyboard="false">Add Project</button>
         <!-- .modal -->
         <div class="modal fade" id="MymodalBack">
         <div class="modal-dialog">
@@ -213,8 +213,7 @@
             <div class="modal-body">
                 <form action="/addProject.php" onsubmit="newProject()">
                 <div class="form-group">
-                    <input type="text" class="form-control" id="inputProjectName" placeholder="Name">
-                    <small id="projectHelpName" class="form-text text-muted">Max characters 100.</small>
+                    <input type="text" class="form-control" id="inputProjectName" placeholder="">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
