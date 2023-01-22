93a392d94aa7e626840aab66940124381cd84079
Vezeveer <mortalmanny@live.com>
2023-01-22 14:11:56 +0800

    small screen cleanup

--- navigation.php
+++ navigation.php
@@ -9,18 +9,33 @@
     </a>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#top">... <span class="sr-only">(current)</span></a>
+            <li class="nav-item d-sm-block d-md-none active">
+                <a class="nav-link" href="#top"><i class="fas fa-home"></i> Dashboard<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item d-sm-block d-md-none">
+                <a class="nav-link" href="#top"><i class="fas fa-sign-out-alt"></i> History</a>
+            </li>
+            <li class="nav-item d-sm-block d-md-none">
+                <a class="nav-link" href="#top"><i class="fas fa-tasks"></i> Current Plan</a>
+            </li>
+            <li class="nav-item d-sm-block d-md-none">
+                <a class="nav-link" href="#top"><i class="fas fa-calendar"></i> Logs</a>
+            </li>
+            <li class="nav-item d-sm-block d-md-none">
+                <a class="nav-link" href="#top"><i class="fas fa-search"></i> Search</a>
+            </li>
+            <li class="nav-item d-sm-block d-md-none">
+                <a class="nav-link" href="#top"><i class="fas fa-user-plus"></i> Create User</a>
+            </li>
+            <li class="nav-item d-sm-block d-md-none">
+                <a class="nav-link" href="includes/logout.inc.php"><i class="fas fa-sign-out-alt"></i> Sign Out</a>
             </li>
             <!-- <li class="nav-item">
-                <a class="nav-link" href="#top">Features</a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link" href="#top">Pricing</a>
             </li> -->
             <!-- This menu is hidden in bigger devices with d-sm-none. 
            The sidebar isn't proper for smaller screens imo, so this dropdown menu can keep all the useful sidebar itens exclusively for smaller screens  -->
-            <li class="nav-item dropdown d-sm-block d-md-none">
+            <!-- <li class="nav-item dropdown d-sm-block d-md-none">
                 <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Menu </a>
                 <div class="dropdown-menu" aria-labelledby="smallerscreenmenu">
                     <a class="dropdown-item" href="includes/logout.inc.php">Sign Out</a>
@@ -28,7 +43,8 @@
                     <a class="dropdown-item" href="#top">Tasks</a>
                     <a class="dropdown-item" href="#top">Etc ...</a>
                 </div>
-            </li><!-- Smaller devices menu END -->
+            </li> -->
+            <!-- Smaller devices menu END -->
         </ul>
     </div>
 </nav><!-- NavBar END -->
