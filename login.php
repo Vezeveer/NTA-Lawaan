da610edff89359a719323514313afe659050c2dd
Vezeveer <mortalmanny@live.com>
2023-01-25 16:39:48 +0800

    login ui overhaul

--- login.php
+++ login.php
@@ -2,32 +2,25 @@
 
 <img class="wave" src="img/wave.png">
 	<div class="container_login">
-		<div class="img">
+		<!-- <div class="img">
 			<img src="img/undraw_visionary_technology_re_jfp7.svg">
-		</div>
+		</div> -->
 		<div class="login-content">
-			<form action="includes1/login.inc.php" method="post">
-				<img class="threeD" src="img/nta_logo_small.png">
-				<h2 class="title" style="color: white; text-shadow: 2px 2px 4px #000000;">Welcome</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Username</h5>
-           		   		<input name="username" type="text" class="input" required>
-           		   </div>
+			<form class="login" action="includes1/login.inc.php" method="post">
+				<!-- <img class="" src="img/nta_logo_small.png" style="width: 100px;"> -->
+				<!-- <h2 class="title" style="color: white; text-shadow: 2px 2px 4px #000000;">Welcome</h2> -->
+           		<div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="fas fa-user"></i></span>
+                    </div>
+           		   		<input name="username" type="text" class="form-control" placeholder="username" required>
            		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Password</h5>
-           		    	<input name="password" type="password" class="input" required>
-            	   </div>
-            	</div>
-            	<a href="#">Forgot Password?</a>
+                   <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="fas fa-lock"></i></span>
+                    </div>
+           		   		<input name="password" type="password" class="form-control" placeholder="password" required>
+           		</div>
 				<?php
 					if(isset($_GET["error"])){
 						if($_GET["error"] == "emptyinput"){
@@ -46,6 +39,7 @@
 				?>
 				
             	<button type="submit" name="submit" class="btn">LOGIN</button>
+                <a href="#" style="display:block;">Forgot Password?</a>
             </form>
         </div>
     </div>
--- php-components/header.php
+++ php-components/header.php
@@ -52,6 +52,7 @@ $_SESSION['adminAccess'] = false;
     <link rel="stylesheet" href="css-libs/bootstrap-datepicker.min.css">
 
     <link rel="stylesheet" type="text/css" href="css/tableResizable.css">
+    <link rel="stylesheet" href="css/login.css">
     <!-- <link rel="stylesheet" type="text/css" href="css/priority.css">
     <link rel="stylesheet" href="css/popup.css"> -->
     <link rel="stylesheet" type="text/css" href="css/sidebar.css">
